5849ab7052e04c8b167536680a4e55d41222641e
lenston19 <lenston190900@gmail.com>
2024-08-15 01:50:35 +0300

    feat: Добавлены middleware

---
+++ middleware/empty-organization-id.ts
@@ -0,0 +1,10 @@
+export default defineNuxtRouteMiddleware((to) => {
+	if (import.meta.client) {
+		const accessToken = new URLSearchParams(to.hash.slice(1)).get(
+			"access_token"
+		)
+		if (!accessToken) {
+			return navigateTo('/settings')
+		}
+	}
+})
