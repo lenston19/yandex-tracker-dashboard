91be76dc51e4ee564b25c143fafc5621b669524e
lenston19 <lenston190900@gmail.com>
2024-08-15 01:57:16 +0300

    feat: Компонент диаграммы работы за месяц

--- components/charts/LineChart.vue
+++ components/charts/LineChart.vue
@@ -1,25 +1,14 @@
-<!-- <script setup lang="ts">
+<script setup lang="ts">
 import {
 	Chart as ChartJS,
 	CategoryScale,
 	LinearScale,
 	PointElement,
 	LineElement,
-	Tooltip
-} from 'chart.js'
-import { Line } from 'vue-chartjs'
-
-export interface LineChartData {
-	labels: string[];
-	datasets: {
-			backgroundColor: string,
-			borderColor: string,
-			borderWidth: number,
-			hoverBackgroundColor: string,
-			hoverBorderColor: string,
-			data: number[];
-	}[];
-}
+	Tooltip,
+} from "chart.js"
+import { Line } from "vue-chartjs"
+import { LineChartData } from "~/types/base";
 
 ChartJS.register(
 	CategoryScale,
@@ -30,36 +19,30 @@ ChartJS.register(
 )
 
 defineProps<{
-  data: LineChartData
+	data: LineChartData
 }>()
 
 const options = computed(() => ({
-  responsive: true,
-  scales: {
-    y: {
-      stacked: true,
-      grid: {
-        display: true,
-        color: 'rgba(56,142,60,0.2)'
-      }
-    },
-    x: {
-      grid: {
-        display: false
-      }
-    }
-  }
+	responsive: true,
+	scales: {
+		y: {
+			stacked: true,
+			grid: {
+				display: true,
+				color: "rgba(56,142,60,0.2)",
+			},
+		},
+		x: {
+			grid: {
+				display: false,
+			},
+		},
+	},
 }))
 </script>
 
 <template>
-  <Line
-		:data="data"
-		:options="options"
-	/>
+	<Line :data="data" :options="options" />
 </template>
 
-
-<style lang="scss">
-
-</style> -->
+<style lang="scss"></style>
