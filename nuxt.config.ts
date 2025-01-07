036cd87842a62ea6a47fb3e2df0bb65fd533b947
lenston19 <lenston190900@gmail.com>
2025-01-07 03:43:32 +0300

    feat: favicon + webmanifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,29 @@
 // @ts-ignore
 export default defineNuxtConfig({
 	compatibilityDate: '2024-08-09',
+app: {
+		head: {
+			htmlAttrs: {
+				lang: 'ru'
+			},
+			meta: [
+				{ charset: 'utf-8' },
+				{
+					name: 'viewport',
+					content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0'
+				},
+				{
+					name: 'format-detection',
+					content: 'telephone=no, date=no, address=no, email=no'
+				}
+			],
+			link: [
+				{ rel: 'icon', href: '/favicon.ico' },
+				{ rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+				{ rel: 'manifest', href: '/site.webmanifest' }
+			],
+		}
+	},
   typescript: {
 		tsConfig: {
 			compilerOptions: {
