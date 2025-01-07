baa0ac8a2cbf02237df197013850ea10102bdafd
lenston19 <lenston190900@gmail.com>
2025-01-07 03:45:04 +0300

    feat: heroicons сделаны константой

---
+++ helpers/static/heroicons.ts
@@ -0,0 +1,15 @@
+export const enum HEROICONS {
+	CIRCLE_STACK_20_SOLID = 'i-heroicons-circle-stack-20-solid',
+	INFORMATION_CIRCLE = 'i-heroicons-information-circle',
+	MOON_20_SOLID = 'i-heroicons-moon-20-solid',
+	SUN_20_SOLID = 'i-heroicons-sun-20-solid',
+	BARS_3 = 'i-heroicons-bars-3',
+	HOME = 'i-heroicons-home',
+	FOLDER = 'i-heroicons-folder',
+	CHART_BAR = 'i-heroicons-chart-bar',
+	COG = 'i-heroicons-cog',
+	ARROW_LEFT = 'i-heroicons-arrow-left',
+	ARROW_RIGHT = 'i-heroicons-arrow-right',
+	ARROW_PATH = 'i-heroicons-arrow-path',
+	ARROW_RIGHT_START_ON_RECTANGLE = 'i-heroicons-arrow-right-start-on-rectangle',
+}
