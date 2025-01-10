9b40868f2c0a38a1da5eaccbcba4f1df6cd61412
lenston19 <lenston190900@gmail.com>
2025-01-10 19:35:42 +0300

    refactor: MainLayout разбит на компоненты

---
+++ components/app/AppLogin.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const config = useRuntimeConfig()
+
+const getUserAccessToken = () => {
+	window.open(
+		`https://oauth.yandex.ru/authorize?response_type=token&client_id=${config.public.yandexClientId}`,
+		"_blank"
+	)
+}
+</script>
+
+<template>
+	<UButton
+		color="red"
+		@click="getUserAccessToken"
+		label="Войти через Яндекс"
+		class="h-fit"
+	/>
+</template>
---
+++ components/app/AppUser.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useAuthStore } from "~/stores/auth"
+
+const { userName, userAvatarUrl } = storeToRefs(useAuthStore())
+</script>
+
+<template>
+	<UButton
+		color="gray"
+		:to="{ name: 'settings' }"
+		class="h-fit"
+	>
+		<UAvatar
+			:src="userAvatarUrl"
+			:alt="userName"
+		/>
+		{{ userName }}
+	</UButton>
+</template>
