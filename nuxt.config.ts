dbfce7f59daca8c103bb350e1df675fe12f4d7c5
lenston19 <lenston190900@gmail.com>
2025-01-13 15:56:10 +0300

    fix: Предварительный рендеринг страницы "Что нового?"

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,6 +43,11 @@ export default defineNuxtConfig({
 			'process.env.DEBUG': false
 		}
 	},
+	nitro: {
+		prerender: {
+			routes: ['/changelog']
+		}
+	},
 	components: [
 		{
 			path: '~/components',
