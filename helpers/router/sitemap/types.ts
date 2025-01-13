04272a6b36566e2a5b86df95e377842b82e3152f
lenston19 <lenston190900@gmail.com>
2025-01-13 10:08:58 +0300

    feat: sitemap

---
+++ helpers/router/sitemap/types.ts
@@ -0,0 +1,12 @@
+import type { RouteLocationNamedRaw } from 'vue-router'
+import type { AppRoute } from '~/helpers/router/types'
+
+export type GetAppRoutePayload = Pick<RouteLocationNamedRaw, 'params' | 'query' | 'hash'>
+type GetAppRoute = (payload: GetAppRoutePayload) => AppRoute
+
+export interface SitemapRoute {
+	name: string
+	icon?: string
+	route: AppRoute
+	getRoute?: GetAppRoute
+}
