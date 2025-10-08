2e1170f4b6f4d93b5834881dac8c699ca8319c2a
lenston <lenston190900@gmail.com>
2025-10-08 21:10:23 +0300

    refactor(error-page): Поправлена визуальная составляющая страницы ошибки

--- app/error.vue
+++ app/error.vue
@@ -12,22 +12,20 @@ const errorCode = props.error?.statusCode ?? 404
 </script>
 
 <template>
-  <div
-    class="flex min-h-svh flex-col items-center justify-center bg-gradient-to-br from-purple-100 to-pink-100 p-6 text-center"
-  >
+  <div class="flex min-h-svh flex-col items-center justify-center bg-default p-6 text-center">
     <img
       src="/gif/Hello There.gif"
       alt="error illustration"
-      class="mx-auto mb-8 w-64"
+      class="mx-auto mb-8 w-full max-w-184"
     />
 
-    <div class="animate-pulse text-3xl font-extrabold text-purple-600">
+    <div class="animate-bounce text-[96px] font-extrabold text-secondary-600">
       {{ errorCode }}
     </div>
 
-    <h1 class="mt-4 text-xl font-bold text-gray-800">Страница не найдена</h1>
+    <h1 class="mt-4 text-xl font-bold text-default">Страница не найдена</h1>
 
-    <p class="mt-2 max-w-md text-gray-600">
+    <p class="mt-2 max-w-md text-default">
       К сожалению, страница, которую вы ищете, не существует или была перемещена.
     </p>
 
