e334665983dcde63db261a63ac8208e3d8abe633
lenston <lenston190900@gmail.com>
2026-05-03 02:13:28 +0300

    fix: адаптация компонентов под новую версию Nuxt UI

--- app/core/components/ui/ui-empty-state.vue
+++ app/core/components/ui/ui-empty-state.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { HEROICONS } from '~/core/constants/heroicons'
+
+const { title = 'Нет данных' } = defineProps<{ title?: string }>()
 </script>
 
 <template>
   <div class="flex min-h-32 flex-col items-center justify-center gap-4">
     <u-icon
       :name="HEROICONS.CIRCLE_STACK_20_SOLID"
-      class="h-6 w-6"
+      class="size-6"
     />
-    <div class="text-h4">Нет данных</div>
+    <div class="text-sm font-medium text-muted">{{ title }}</div>
   </div>
 </template>
