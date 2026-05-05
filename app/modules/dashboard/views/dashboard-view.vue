773542eb7b3257435d481a7f3014fd3182a383d5
lenston <lenston190900@gmail.com>
2026-05-05 23:05:53 +0300

    feat(dashboard): Гибкая сетка виджетов на главной

--- app/modules/dashboard/views/dashboard-view.vue
+++ app/modules/dashboard/views/dashboard-view.vue
@@ -1,29 +1,101 @@
 <script setup lang="ts">
+import { computed, defineAsyncComponent, type Component } from 'vue'
 import WeekTimeWidget from '../components/week-time-widget.vue'
 import DayTimeWidget from '../components/day-time-widget.vue'
 import MonthTimeWidget from '../components/month-time-widget.vue'
-import ActivityHeatmapWidget from '../components/activity-heatmap-widget.vue'
-import MyIssuesWidget from '../components/my-issues-widget.vue'
-import { SITEMAP } from '~/core/utils/router/sitemap/index'
 import { useSiteSettingsStore } from '~/modules/settings'
+import { useIsMobile } from '~/core/composables/use-is-mobile'
 
-useHead({ title: SITEMAP.main.name })
+const ActivityHeatmapWidget = defineAsyncComponent(() => import('../components/activity-heatmap-widget.vue'))
+const MyIssuesWidget = defineAsyncComponent(() => import('../components/my-issues-widget.vue'))
 
 const { heatmap, myIssues } = storeToRefs(useSiteSettingsStore())
+
+const layout = computed(() => {
+  const left: Component[] = []
+  const right: Component[] = []
+
+  const top: Component[] = [WeekTimeWidget]
+
+  if (myIssues.value.show) {
+    left.push(DayTimeWidget, MonthTimeWidget)
+
+    if (heatmap.value.show) {
+      left.push(ActivityHeatmapWidget)
+    }
+
+    right.push(MyIssuesWidget)
+  } else {
+    left.push(DayTimeWidget)
+
+    if (heatmap.value.show) {
+      left.push(ActivityHeatmapWidget)
+    }
+
+    right.push(MonthTimeWidget)
+  }
+
+  return { top, left, right }
+})
+
+const mobileOrder = computed(() => {
+  const list: Component[] = []
+
+  list.push(DayTimeWidget)
+  list.push(MonthTimeWidget)
+  list.push(WeekTimeWidget)
+
+  if (myIssues.value.show) {
+    list.push(MyIssuesWidget)
+  }
+
+  if (heatmap.value.show) {
+    list.push(ActivityHeatmapWidget)
+  }
+
+  return list
+})
+
+const isMobile = useIsMobile()
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-3">
-    <week-time-widget class="order-3 col-span-4 md:order-1" />
-    <day-time-widget class="order-1 col-span-4 md:order-2 md:col-span-2" />
-    <month-time-widget class="order-2 col-span-4 md:order-3 md:col-span-2" />
-    <my-issues-widget
-      v-if="myIssues.show"
-      class="order-4 col-span-4 md:col-span-2"
+  <div
+    v-if="isMobile"
+    class="flex flex-col gap-3 md:hidden"
+  >
+    <component
+      :is="comp"
+      v-for="(comp, i) in mobileOrder"
+      :key="'m-' + i"
     />
-    <activity-heatmap-widget
-      v-if="heatmap.show"
-      class="order-5 col-span-4 md:col-span-2"
+  </div>
+
+  <div
+    v-else
+    class="hidden grid-cols-2 gap-3 md:grid"
+  >
+    <component
+      :is="comp"
+      v-for="(comp, i) in layout.top"
+      :key="'top-' + i"
+      class="col-span-2"
     />
+
+    <div class="flex flex-col gap-3">
+      <component
+        :is="comp"
+        v-for="(comp, i) in layout.left"
+        :key="'left-' + i"
+      />
+    </div>
+
+    <div class="flex flex-col gap-3">
+      <component
+        :is="comp"
+        v-for="(comp, i) in layout.right"
+        :key="'right-' + i"
+      />
+    </div>
   </div>
 </template>
