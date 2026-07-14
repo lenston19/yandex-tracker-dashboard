19d6c5799ce33a5022ab48a66deec8314b5ec0a5
lenston <lenston190900@gmail.com>
2026-07-14 21:24:49 +0300

    refactor(storage): добавлен префикс ytd: для ключей куков и localStorage с миграцией данных

--- app/core/store/use-app-version-store.ts
+++ app/core/store/use-app-version-store.ts
@@ -1,5 +1,16 @@
+import { storageKey } from '~/core/constants/storage-keys'
+
 export const useAppVersionStore = defineStore('app-version', () => {
-  const appVersion = useCookie('app-version')
+  const appVersion = useCookie(storageKey('app-version'))
+
+  // Обратная совместимость с прошлыми значениями
+  if (import.meta.client) {
+    const oldAppVersion = useCookie('app-version')
+    if (oldAppVersion.value && !appVersion.value) {
+      appVersion.value = oldAppVersion.value
+      oldAppVersion.value = null
+    }
+  }
 
   function setVersion(version: string) {
     appVersion.value = version
